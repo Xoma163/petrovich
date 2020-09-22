7d7ceea63d8f06e2f37c88868f9d2af8a5ab17b0
Xoma163 <shaposhnikov@akticom.ru>
2020-09-23 01:26:15 +0400

    fixed digits in ER

--- apps/bot/commands/ExchangeRates.py
+++ apps/bot/commands/ExchangeRates.py
@@ -30,13 +30,13 @@ class ExchangeRates(CommonCommand):
             if any(ext in currency for ext in ['rub', "руб"]):
                 msg = "Перевод в другие валюты:\n"
                 for ex_rate in ex_rates:
-                    total_value = round(value / ex_rates[ex_rate]['value'], 2)
+                    total_value = self.to_fixed(value / ex_rates[ex_rate]['value'], 2)
                     msg += f"{total_value} {ex_rate}\n"
                 return msg
             else:
                 for code in ex_rates:
                     if currency[:5] in ex_rates[code]['name'] or currency in code.lower():
-                        total_value = round(value * ex_rates[code]['value'], 2)
+                        total_value = self.to_fixed(value * ex_rates[code]['value'], 2)
                         msg = "Перевод в рубли:\n"
                         msg += f"{total_value} руб."
                         return msg
@@ -45,6 +45,10 @@ class ExchangeRates(CommonCommand):
         else:
             msg = "Курс валют:\n"
             for ex_rate in ex_rates:
-                ex_rates[ex_rate]['value'] = round(ex_rates[ex_rate]['value'], 2)
+                ex_rates[ex_rate]['value'] = self.to_fixed(ex_rates[ex_rate]['value'], 2)
                 msg += f"{ex_rate} - {ex_rates[ex_rate]['value']} руб.\n"
             return msg
+
+    @staticmethod
+    def to_fixed(num, digits=0):
+        return f"{num:.{digits}f}"
