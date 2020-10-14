5fb739a81461a8f255210eb042b3304ceec08c33
Xoma163 <shaposhnikov@akticom.ru>
2020-10-14 23:40:37 +0400

    AmongUs

---
+++ apps/bot/commands/TrustedCommands/AmongUs.py
@@ -0,0 +1,19 @@
+from apps.bot.classes.Consts import Role
+from apps.bot.classes.common.CommonCommand import CommonCommand
+
+
+class AmongUs(CommonCommand):
+    def __init__(self):
+        names = ["amongus", "амонгас", "among", 'амонг']
+        help_text = "Amongus - зовёт всех играть"
+        super().__init__(names, help_text, access=Role.TRUSTED)
+
+    def start(self):
+        user_notifications = "@xoma163\n" \
+                             "@lana.chern\n" \
+                             "@ma_maksim\n" \
+                             "@eatm0re\n" \
+                             "@mstop\n" \
+                             "@e.korsakov\n" \
+                             "@red_phenyk\n"
+        return user_notifications
