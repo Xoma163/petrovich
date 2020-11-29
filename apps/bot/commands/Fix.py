49a3d7464c71ba1e097df16bcdfc4243d6e79bc9
AndrewSha <Xoma163rus@gmail.com>
2020-11-29 10:34:55 +0400

    fix fix :D

--- apps/bot/commands/Fix.py
+++ apps/bot/commands/Fix.py
@@ -19,14 +19,20 @@ class Fix(CommonCommand):
     def __init__(self):
         names = ["фикс", "раскладка"]
         help_text = "Фикс - исправляет раскладку текста"
-        detail_help_text = "Фикс (Пересылаемые сообщения) - исправляет раскладку текста"
-        super().__init__(names, help_text, detail_help_text, fwd=True, platforms=[Platform.VK, Platform.TG])
+        detail_help_text = "Фикс (Пересылаемые сообщения) - исправляет раскладку текста\n" \
+                           "Фикс (текст) - исправляет раскладку текста"
+        super().__init__(names, help_text, detail_help_text, platforms=[Platform.VK, Platform.TG])
 
     def start(self):
-        msgs = ""
-        for msg in self.event.fwd:
-            if msg['text']:
-                msgs += f"{fix_layout(msg['text'], has_cyrillic(msg['text']))}\n"
-        if not msgs:
-            raise RuntimeWarning("Нет текста в сообщении или пересланных сообщениях")
+        if self.event.args:
+            msgs = fix_layout(self.event.original_args, has_cyrillic(self.event.original_args))
+        elif self.event.fwd:
+            msgs = ""
+            for msg in self.event.fwd:
+                if msg['text']:
+                    msgs += f"{fix_layout(msg['text'], has_cyrillic(msg['text']))}\n"
+            if not msgs:
+                raise RuntimeWarning("Нет текста в сообщении или пересланных сообщениях")
+        else:
+            raise RuntimeWarning("Перешлите сообщение или укажите текст в аргументах команды")
         return msgs
