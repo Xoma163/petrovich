4759a1a5b847cad6c839318d7817c152bfc161c7
AndrewSha <Xoma163rus@gmail.com>
2020-12-19 10:45:30 +0400

    fix suggest

--- apps/bot/commands/EasyCommands/Clear.py
+++ apps/bot/commands/EasyCommands/Clear.py
@@ -5,7 +5,7 @@ from apps.bot.classes.common.CommonMethods import random_event
 class Clear(CommonCommand):
     def __init__(self):
         names = ["ясно", "ммм"]
-        super().__init__(names)
+        super().__init__(names, suggest_for_similar=False)
 
     def start(self):
         if self.event.command == 'ммм':
