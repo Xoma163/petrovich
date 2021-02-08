7ee373046df697a9d4dcf0d3106859e7a99f1567
AndrewSha <Xoma163rus@gmail.com>
2021-02-08 23:08:06 +0400

    Images finder

--- apps/bot/commands/Find.py
+++ apps/bot/commands/Find.py
@@ -1,7 +1,12 @@
-from apps.bot.APIs.QwantAPI import QwantAPI
+import json
+
+import requests
+from bs4 import BeautifulSoup
+
 from apps.bot.classes.Consts import Platform
 from apps.bot.classes.Exceptions import PWarning
 from apps.bot.classes.common.CommonCommand import CommonCommand
+from apps.bot.classes.common.CommonMethods import check_command_time
 
 
 class Find(CommonCommand):
@@ -13,14 +18,20 @@ class Find(CommonCommand):
         super().__init__(names, help_text, detail_help_text, args=1, platforms=[Platform.VK, Platform.TG])
 
     def start(self):
+        # В связи с тем, что парсим страницы
+        check_command_time("find_images", 15)
         self.bot.set_activity(self.event.peer_id)
 
         query = self.event.original_args
         count = 5
 
-        qwant_api = QwantAPI()
-        urls = qwant_api.get_urls(query)
+        content = requests.get(f'https://yandex.ru/images/search?text={query}').content
+        bs4 = BeautifulSoup(content)
+        urls = [json.loads(x.attrs['data-bem'])['serp-item']['img_href'] for x in bs4.select('.serp-item')]
 
+        # qwant_api = QwantAPI()
+        # urls = qwant_api.get_urls(query)
+        #
         if len(urls) == 0:
             raise PWarning("Ничего не нашёл")
         attachments = self.bot.upload_photos(urls, count)
