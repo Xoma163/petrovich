f42baacbbb46df25b73c55aa59536872e9a7ec38
AndrewSha <Xoma163rus@gmail.com>
2021-08-20 01:55:42 +0400

    code cleanup

--- apps/bot/admin.py
+++ apps/bot/admin.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 from apps.bot.models import Users, Chat, Bot
 
 
+@admin.register(Users)
 class UserAdmin(admin.ModelAdmin):
     list_display = (
-    'show_user_id', 'show_url', 'name', 'surname', 'nickname', 'nickname_real', 'gender', 'birthday', 'city')
+        'show_user_id', 'show_url', 'name', 'surname', 'nickname', 'nickname_real', 'gender', 'birthday', 'city')
     list_display_links = ('show_user_id',)
     fieldsets = (
         ('Информация о пользователе', {
@@ -26,20 +27,13 @@ class UserAdmin(admin.ModelAdmin):
     search_fields = ['name', 'surname', 'nickname', 'nickname_real', 'user_id']
 
 
-admin.site.register(Users, UserAdmin)
-
-
+@admin.register(Chat)
 class ChatAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'admin', 'need_reaction', 'platform','is_banned')
+    list_display = ('id', 'name', 'admin', 'need_reaction', 'platform', 'is_banned')
     list_filter = ('platform',)
 
 
-admin.site.register(Chat, ChatAdmin)
-
-
+@admin.register(Bot)
 class BotAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'platform',)
     list_filter = ('platform',)
-
-
-admin.site.register(Bot, BotAdmin)
