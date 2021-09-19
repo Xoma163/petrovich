82e5fe20c97a4d202e6759cdce5a59a16e2643fc
AndrewSha <Xoma163rus@gmail.com>
2021-09-19 22:18:15 +0400

    upd_production

---
+++ update_production.sh
@@ -0,0 +1,13 @@
+git checkout master
+git reset HEAD --hard
+git clean -fd
+git pull
+
+#npm run build
+venv/bin/python -m pip install --upgrade pip setuptools wheel
+venv/bin/pip install -r requirements.txt
+venv/bin/python manage.py migrate
+venv/bin/python manage.py collectstatic --noinput
+
+sudo systemctl restart petrovich
+sudo systemctl restart petrovich_site
