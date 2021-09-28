dea8bf6af85a37f4e23295fcd0fc3ccd59f5810a
AndrewSha <Xoma163rus@gmail.com>
2021-09-28 22:20:36 +0400

    Google Custom Search engine God bless you

---
+++ apps/bot/APIs/GoogleCustomSearchAPI.py
@@ -0,0 +1,25 @@
+import requests
+
+from petrovich.settings import env
+
+
+class GoogleCustomSearchAPI:
+    URL = "https://customsearch.googleapis.com/customsearch/v1?"
+
+    def __init__(self):
+        pass
+
+    def get_images_urls(self, query):
+        querystring = {
+            "key": env.str("GOOGLE_CUSTOM_SEARCH_API_KEY"),
+            "cx": env.str("GOOGLE_SEARCH_ENGINE_ID"),
+            "searchType": "image",
+            "safe": "active",
+            "q": query
+        }
+
+        response = requests.get(self.URL, params=querystring).json()
+        images_urls = []
+        if 'items' in response:
+            images_urls = [x['link'] for x in response['items']]
+        return images_urls
--- apps/bot/commands/Find.py
+++ apps/bot/commands/Find.py
@@ -1,13 +1,9 @@
-import json
 from urllib.parse import urlparse
 
-import requests
-from bs4 import BeautifulSoup
-
+from apps.bot.APIs.GoogleCustomSearchAPI import GoogleCustomSearchAPI
 from apps.bot.classes.Consts import Platform
 from apps.bot.classes.Exceptions import PWarning
 from apps.bot.classes.common.CommonCommand import CommonCommand
-from apps.bot.classes.common.CommonMethods import check_command_time
 
 
 class Find(CommonCommand):
@@ -19,25 +15,14 @@ class Find(CommonCommand):
     platforms = [Platform.VK, Platform.TG]
 
     def start(self):
-        # В связи с тем, что парсим страницы
-        check_command_time("find_images", 15)
         self.bot.set_activity(self.event.peer_id)
 
         query = self.event.original_args
         count = 5
 
-        content = requests.get(
-            f'https://yandex.ru/images/search?text={query}',
-            # Кука на безопасный поиск
-            cookies={
-                'yp': '1929040280.sp.family%3A2#1613420360.szm.1%3A1920x1080%3A1920x969#1612901961.nps.7769161127%3Aclose'
-            }).content
-        bs4 = BeautifulSoup(content, 'html.parser')
-        urls = [json.loads(x.attrs['data-bem'])['serp-item']['img_href'] for x in bs4.select('.serp-item')]
+        gcs_api = GoogleCustomSearchAPI()
+        urls = gcs_api.get_images_urls(query)
 
-        # qwant_api = QwantAPI()
-        # urls = qwant_api.get_urls(query)
-        #
         for i, url in enumerate(urls):
             parsed_url = urlparse(url)
             urls[i] = f"{parsed_url.scheme}://{parsed_url.hostname}{parsed_url.path}"
