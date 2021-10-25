72f3c3633d014f3ff218f25e53a51aecd5fcfd85
AndrewSha <Xoma163rus@gmail.com>
2021-10-26 01:34:41 +0400

    Device/Devices/Sensors -> Home added openhab3 API (yahoo)

---
+++ apps/bot/APIs/Openhab3API.py
@@ -0,0 +1,67 @@
+import requests
+
+from apps.bot.classes.consts.Exceptions import PWarning
+from petrovich.settings import env
+
+
+class Openhab3API:
+    URL = env.str("OPENHAB3_API_URL")
+    TOKEN = env.str("OPENHAB3_API_TOKEN")
+    HEADERS = {
+        "Accept": "application/json",
+        "Authorization": f"Bearer {TOKEN}"
+    }
+
+    def get_items(self, room=None):
+        params = {'recursive': True}
+        items = requests.get(f"{self.URL}/items", params, headers=self.HEADERS).json()
+        if 'error' in items:
+            raise PWarning("Ошибка")
+        if room:
+            items = list(filter(lambda x: room in x['groupNames'], items))
+        return self._strip_items_fields(items)
+
+    @staticmethod
+    def _strip_items_fields(items):
+        parsed_items = []
+        for item in items:
+            parsed_item = {
+                'name': item['name'],
+                'label': item['label'],
+                'type': item['type'],
+                'category': item['category'],
+                'sub_items': []}
+            sub_items = item.get('members', [])
+            for sub_item in sub_items:
+                parsed_sub_item = {
+                    'name': sub_item['name'],
+                    'label': sub_item['label'],
+                    'type': sub_item['type'],
+                    'state': sub_item['state'],
+                }
+                if sub_item.get('stateDescription'):
+                    parsed_sub_item['uom'] = sub_item['stateDescription']['pattern'].replace("%s", '').replace("%%",
+                                                                                                               "%")
+                parsed_item['sub_items'].append(parsed_sub_item)
+            parsed_items.append(parsed_item)
+        return parsed_items
+
+    def set_item_state(self, item_name, state):
+        items = self.get_items()
+        item = self.find_item_by_label(items, item_name)
+
+        state = "ON" if state else "OFF"
+        headers = {"Content-Type": "text/plain"}
+        headers.update(self.HEADERS)
+        res = requests.post(f"{self.URL}/items/{item['name']}/", data=state, headers=headers)
+        if res.status_code != 200:
+            raise RuntimeError("Ошибка")
+
+    @staticmethod
+    def find_item_by_label(items, label):
+        label_lower = label.lower()
+        for item in items:
+            for sub_item in item['sub_items']:
+                if sub_item['label'].lower() == label_lower:
+                    return sub_item
+        raise RuntimeWarning(f"Не нашёл устройства с именем {label}")
---
+++ apps/bot/commands/HomeCommands/Home.py
@@ -0,0 +1,107 @@
+from apps.bot.APIs.Openhab3API import Openhab3API
+from apps.bot.classes.Command import Command
+from apps.bot.classes.consts.Consts import Role, ON_OFF_TRANSLATOR
+from apps.bot.classes.consts.Exceptions import PWarning
+
+
+class Home(Command):
+    name = "дом"
+    help_text = "значение датчиков и состояние устройств в доме"
+    help_texts = [
+        "- значение датчиков и состояние устройств в доме",
+        "(комната) - значение датчиков и состояние устройств в комнате",
+        "(устройство) (вкл/выкл) - управление устройством в доме"
+    ]
+    access = Role.HOME
+
+    ROOMS = {
+        'LivingRoom': {
+            'name': "Гостиная",
+            'items_names': [
+                'Humidifier',
+                'RGB_strip',
+                'Fan',
+                'Meteostation1',
+                'heater_in_legs'
+            ]
+        },
+        'Bedroom': {
+            'name': "Спальня",
+            'items_names': [
+                "Heater",
+                "Meteostation2"
+            ]
+        },
+        'Bathroom': {
+            'name': "Ванная",
+            'items_names': [
+                "meteostation_3"
+            ]
+        }
+    }
+
+    def start(self):
+        if not self.event.message.args:
+            return self.get_home_items()
+        else:
+            try:
+                room = self.get_room_by_name(self.event.message.args[0])
+                return self.get_room_items(room)
+            except PWarning:
+                self.args = 2
+                self.check_args()
+
+            return self.set_item_state(self.event.message.args[:-1], self.event.message.args[-1])
+
+    def get_home_items(self):
+        msgs = []
+        oh3_api = Openhab3API()
+        items = oh3_api.get_items()
+        for room in self.ROOMS:
+            data = self.ROOMS[room]
+            room_name = data['name']
+            room_items = self.filter_room_items(items, room)
+            room_msg = f"{room_name}:\n{self.prepare_items_to_msg(room_items)}"
+            msgs.append(room_msg)
+        return f"{'-' * 40}\n".join(msgs)
+
+    def get_room_items(self, room):
+        oh3 = Openhab3API()
+        items = oh3.get_items(room)
+        items = self.filter_room_items(items, room)
+        return self.prepare_items_to_msg(items)
+
+    def filter_room_items(self, items, room):
+        return list(filter(lambda x: x['name'] in self.ROOMS[room]['items_names'], items))
+
+    @staticmethod
+    def set_item_state(item_name, state):
+        state_translated = ON_OFF_TRANSLATOR.get(state)
+        if state_translated is None:
+            raise PWarning(f"Я не знаю состояния {state}")
+        item_name = " ".join(item_name)
+        oh3 = Openhab3API()
+        oh3.set_item_state(item_name, state_translated)
+        return f"Поменял состояние на {state}"
+
+    def get_room_by_name(self, arg):
+        for k in self.ROOMS:
+            data = self.ROOMS[k]
+            if arg == data['name'].lower():
+                return k
+        raise PWarning(f"Не нашёл комнаты {arg}")
+
+    @staticmethod
+    def prepare_items_to_msg(items):
+        msgs = []
+        for item in items:
+            item_msg = f"{item['label']}:\n"
+            for sub_item in item['sub_items']:
+                if item['category'] == 'sensors':
+                    if sub_item.get("uom"):
+                        item_msg += f'{sub_item["label"]} - {sub_item["state"]}{sub_item["uom"]}\n'
+                elif sub_item['type'] == 'Switch':
+                    item_msg += f'{sub_item["label"]} - {sub_item["state"]}\n'
+
+            msgs.append(item_msg)
+        return "\n".join(msgs)
