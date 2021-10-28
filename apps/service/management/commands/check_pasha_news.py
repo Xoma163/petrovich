78576e9fdecef72fcc2a8c74c2fd3fafb64e2bc1
AndrewSha <Xoma163rus@gmail.com>
2021-10-28 20:23:07 +0400

    check pasha news

---
+++ apps/service/management/commands/check_pasha_news.py
@@ -0,0 +1,49 @@
+import requests
+from bs4 import BeautifulSoup
+from django.core.management.base import BaseCommand
+
+from apps.bot.classes.bots.Bot import get_bot_by_platform
+from apps.bot.models import Users
+from apps.service.models import Service
+
+
+class Command(BaseCommand):
+    URL = "https://gks1frunze.ru"
+
+    def add_arguments(self, parser):
+        parser.add_argument('chat_id', nargs='+', type=str, help='chat_id')
+
+    def handle(self, *args, **options):
+        chat_id = options['chat_id'][0]
+        pasha = Users.objects.get(user_id=chat_id)
+        bot = get_bot_by_platform(pasha.get_platform_enum())
+
+        pasha_news_last_id_entity, created = Service.objects.get_or_create(
+            name='pasha_news_last_id',
+            defaults={'value': 0}
+        )
+        pasha_news_last_id = int(pasha_news_last_id_entity.value)
+        content = requests.get(f"{self.URL}/news").content
+        bs4 = BeautifulSoup(content, "html.parser")
+        news = list(bs4.select(".news-list > .row > a"))
+
+        pasha_news_last_id -= 1
+        if created:
+            last_news_id = news[0].attrs['href'].split('/')[-2]
+            pasha_news_last_id_entity.value = last_news_id
+            pasha_news_last_id_entity.save()
+            return
+
+        news_to_send = list(filter(lambda x: int(x.attrs['href'].split('/')[-2]) > pasha_news_last_id, news))
+        msgs = []
+        for news in news_to_send:
+            msgs.append(self.parse_news(f"{self.URL}{news.attrs['href']}"))
+
+        for msg in msgs:
+            bot.parse_and_send_msgs(pasha.user_id, msg)
+
+    def parse_news(self, news_url):
+        content = requests.get(news_url).content
+        bs4 = BeautifulSoup(content, "html.parser")
+        news_content = bs4.select(".news-detail .col-lg-9")[0].text.strip()
+        return f"{news_content}\n\n{news_url}"
