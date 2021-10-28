2a961308bf1b1e882e1146461d2159a3bf4a8660
AndrewSha <Xoma163rus@gmail.com>
2021-10-28 20:40:32 +0400

    save last news id (check pasha news)

--- apps/service/management/commands/check_pasha_news.py
+++ apps/service/management/commands/check_pasha_news.py
@@ -26,23 +26,31 @@ class Command(BaseCommand):
         content = requests.get(f"{self.URL}/news").content
         bs4 = BeautifulSoup(content, "html.parser")
         news = list(bs4.select(".news-list > .row > a"))
-
         if created:
-            last_news_id = news[0].attrs['href'].split('/')[-2]
+            last_news_id = self.get_news_id(news[0])
             pasha_news_last_id_entity.value = last_news_id
             pasha_news_last_id_entity.save()
             return
 
-        news_to_send = list(filter(lambda x: int(x.attrs['href'].split('/')[-2]) > pasha_news_last_id, news))
+        news_to_send = list(filter(lambda x: self.get_news_id(x) > pasha_news_last_id, news))
         msgs = []
         for news in news_to_send:
             msgs.append(self.parse_news(f"{self.URL}{news.attrs['href']}"))
 
+        last_news_id = self.get_news_id(news_to_send[0])
+        pasha_news_last_id_entity.value = last_news_id
+        pasha_news_last_id_entity.save()
+
         for msg in msgs:
             bot.parse_and_send_msgs(pasha.user_id, msg)
 
-    def parse_news(self, news_url):
+    @staticmethod
+    def parse_news(news_url):
         content = requests.get(news_url).content
         bs4 = BeautifulSoup(content, "html.parser")
         news_content = bs4.select(".news-detail .col-lg-9")[0].text.strip()
         return f"{news_content}\n\n{news_url}"
+
+    @staticmethod
+    def get_news_id(x):
+        return int(x.attrs['href'].split('/')[-2])
