e5dcdf73a8d60091ac0ab3f4989086f04eb10fa7
AndrewSha <Xoma163rus@gmail.com>
2021-10-30 14:31:52 +0400

    added file_id to attachments

--- apps/bot/classes/messages/attachments/Attachment.py
+++ apps/bot/classes/messages/attachments/Attachment.py
@@ -23,6 +23,9 @@ class Attachment:
         # bytes
         self.content = None
 
+        # tg
+        self.file_id = None
+
     def set_private_download_url_tg(self, tg_bot, file_id):
         file_path = tg_bot.requests.get('getFile', params={'file_id': file_id}).json()['result']['file_path']
         self.private_download_url = f'https://{tg_bot.API_TELEGRAM_URL}/file/bot{tg_bot.token}/{file_path}'
--- apps/bot/classes/messages/attachments/PhotoAttachment.py
+++ apps/bot/classes/messages/attachments/PhotoAttachment.py
@@ -13,8 +13,8 @@ class PhotoAttachment(Attachment):
         self.height = event_photo.get('height')
         self.size = event_photo['file_size']
 
-        file_id = event_photo['file_id']
-        self.set_private_download_url_tg(tg_bot, file_id)
+        self.file_id = event_photo['file_id']
+        self.set_private_download_url_tg(tg_bot, self.file_id)
 
     def parse_vk_photo(self, event_photo):
         max_size_photo = sorted(event_photo['sizes'], key=lambda x: x['height'])[-1]
