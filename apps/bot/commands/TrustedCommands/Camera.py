6203cb1abe98944631fbd1df3e6c5c7feee66ca2
AndrewSha <Xoma163rus@gmail.com>
2021-11-05 11:44:01 +0400

    fixed Camera command

--- apps/bot/commands/TrustedCommands/Camera.py
+++ apps/bot/commands/TrustedCommands/Camera.py
@@ -1,6 +1,5 @@
 from apps.bot.classes.Command import Command
 from apps.bot.classes.consts.Consts import Role, Platform
-from apps.bot.classes.consts.Exceptions import PWarning, PError
 from apps.bot.management.commands.start import camera_handler
 
 
@@ -16,34 +15,21 @@ class Camera(Command):
     platforms = [Platform.VK, Platform.TG]
 
     def start(self):
-        attachments = []
-        try:
-            image = camera_handler.get_img()
-        except PWarning as e:
-            print(e)
-            raise PError("какая-то дичь с камерой. Зовите разраба")
-        attachment = self.bot.upload_photos(image, peer_id=self.event.peer_id)[0]
-        attachments.append(attachment)
-
         frames = 100
+        attachments = []
         if self.event.message.args:
             frames = self.event.message.args[0]
             self.check_number_arg_range(frames, 0, camera_handler.MAX_FRAMES)
 
-        if frames != 0:
-            try:
-                document = camera_handler.get_gif(frames)
-            except PError as e:
-                return str(e)
-            attachment = self.bot.upload_video(document, self.event.peer_id, "Камера", filename="camera.gif", )
-            attachments.append(attachment)
-            # if len(attachments) == 2 or self.event.platform == Platform.VK:
-            return {
-                'attachments': attachments,
-                "keyboard": self.bot.get_inline_keyboard(
-                    [{'command': self.name, 'button_text': "Ещё", 'args': [frames]}]),
-            }
-        # else:
-        #     self.bot.parse_and_send_msgs({'attachments': [attachments[0]]}, self.event.peer_id)
-        #     self.bot.parse_and_send_msgs({'attachments': [attachments[1]]}, self.event.peer_id)
-        #     return None
+        if frames == 0:
+            image = camera_handler.get_img()
+            attachments = self.bot.upload_photos(image, peer_id=self.event.peer_id)
+        else:
+            document = camera_handler.get_gif(frames)
+            attachments = [self.bot.upload_video(document, self.event.peer_id, "Камера", filename="camera.gif")]
+        return {
+            'attachments': attachments,
+            "keyboard": self.bot.get_inline_keyboard(
+                [{'command': self.name, 'button_text': "Ещё", 'args': [frames]}]
+            )
+        }
