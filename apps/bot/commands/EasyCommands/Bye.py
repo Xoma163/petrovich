c1a80d25ed41a56d9d1f793a8c02de7a6eb8c448
AndrewSha <Xoma163rus@gmail.com>
2021-12-12 13:23:45 +0400

    mentioned - true

--- apps/bot/commands/EasyCommands/Bye.py
+++ apps/bot/commands/EasyCommands/Bye.py
@@ -5,6 +5,7 @@ from apps.bot.utils.utils import random_event
 class Bye(Command):
     name = "пока"
     names = ["бай", "bb", "бай-бай", "байбай", "бб", "досвидос", "до встречи", "бывай", 'пока-пока', 'пока((']
+    mentioned = True
 
     def start(self):
         return random_event(self.full_names)
--- apps/bot/commands/EasyCommands/Hi.py
+++ apps/bot/commands/EasyCommands/Hi.py
@@ -6,6 +6,7 @@ class Hi(Command):
     name = "привет"
     names = ["хай", "даров", "дарова", "здравствуй", "здравствуйте", "привки", "прив", "q", "qq", "ку",
              "куку", "здаров", "здарова", "хеей", "хало", "hi", "hello", 'салам']
+    mentioned = True
 
     def start(self):
         return random_event(self.full_names)
