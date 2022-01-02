437a460cb3efed19568ed60ef29bf0fce2be86a7
AndrewSha <Xoma163rus@gmail.com>
2022-01-02 11:50:02 +0400

    turret coeff

--- apps/bot/commands/_Service/Turret.py
+++ apps/bot/commands/_Service/Turret.py
@@ -9,7 +9,7 @@ class Turret(Command):
 
     def accept(self, event):
         if event.chat and event.chat.need_turret:
-            chance = 0.02 if event.chat.mentioning else 2
+            chance = 0.4 if event.chat.mentioning else 2
             if random_probability(chance):
                 msg = random_event(TURRET_WORDS)
                 event.bot.parse_and_send_msgs(msg, event.peer_id)
