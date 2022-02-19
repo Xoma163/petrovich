6faa4f4920d0458e2c67add3002a87ec61e24125
AndrewSha <Xoma163rus@gmail.com>
2022-02-19 15:32:49 +0400

    logs filter by chat and level

--- apps/bot/commands/ModeratorCommands/Logs.py
+++ apps/bot/commands/ModeratorCommands/Logs.py
@@ -1,8 +1,10 @@
 import io
 import json
+import logging
 
 from apps.bot.classes.Command import Command
 from apps.bot.classes.consts.Consts import Role, Platform
+from apps.bot.classes.consts.Exceptions import PWarning
 from apps.bot.utils.utils import draw_text_on_image
 from petrovich.settings import DEBUG_FILE
 
@@ -14,7 +16,7 @@ class Logs(Command):
 
     help_text = "логи бота"
     help_texts = [
-        "[кол-во строк=10] - логи. Макс 50 строк."
+        "[уровень логов = DEBUG] [кол-во записей=10] - логи. Макс 30 записей. Возможные уровни логов: DEBUG/INFO/WARNING/ERROR/CRITICAL"
     ]
 
     access = Role.MODERATOR
@@ -23,7 +25,24 @@ class Logs(Command):
     MAX_LOGS_COUNT = 50
 
     def start(self):
-        logs_txt = self.get_bot_logs()
+        count = 10
+
+        level = logging.DEBUG
+        level_name = "DEBUG"
+        if self.event.message.args:
+            level = logging._nameToLevel.get(self.event.message.args[0].upper(), logging._nameToLevel[level_name])
+        self.int_args = [-1]
+        try:
+            self.parse_int()
+            count = self.event.message.args[-1]
+            count = min(count, 30)
+        except PWarning:
+            pass
+
+        filter_levels = [logging._levelToName[x] for x in logging._levelToName if x >= level]
+        filter_chat = self.event.peer_id if self.event.chat else None
+
+        logs_txt = self.get_bot_logs(count, filter_chat, filter_levels)
         img = draw_text_on_image(logs_txt)
         img_byte_arr = io.BytesIO()
         img.save(img_byte_arr, format='PNG')
@@ -57,17 +76,23 @@ class Logs(Command):
             for item in items:
                 self.transform_logs_by_values(item)
 
-    def get_bot_logs(self):
-        count = self.get_count(10, self.MAX_LOGS_COUNT)
-        res = self.read_file(DEBUG_FILE, count)
-        res2 = []
+    def get_bot_logs(self, count, filter_chat=None, filter_level=None):
+        file_rows = self.read_file(DEBUG_FILE)
         separator = "-" * 150
-        for item in res:
-            item_json = json.loads(item)
+        res2 = []
+        for i in range(len(file_rows) - 1, -1, -1):
+            item_json = json.loads(file_rows[i])
+            if filter_chat and str(item_json['event']['peer_id']) != self.event.chat.chat_id:
+                continue
+            if filter_level and item_json['levelname'] not in filter_level:
+                continue
             self.transform_logs_by_values(item_json)
             item_str = json.dumps(item_json, indent=2, ensure_ascii=False)
             res2.append(separator)
             res2.append(item_str)
+
+            if len(res2) > count:
+                break
         text = "\n".join(res2)
         return text
 
@@ -81,7 +106,7 @@ class Logs(Command):
         return default
 
     @staticmethod
-    def read_file(path, rows_count):
+    def read_file(path):
         with open(path, 'r') as file:
-            lines = file.readlines()[-rows_count:]
+            lines = file.readlines()  # [-rows_count:]
         return lines
