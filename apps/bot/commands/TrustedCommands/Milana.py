23be03e7f6e12978dcf9363d79b239678c460fa3
AndrewSha <Xoma163rus@gmail.com>
2022-04-27 09:13:42 +0400

    no symbols

--- apps/bot/commands/TrustedCommands/Milana.py
+++ apps/bot/commands/TrustedCommands/Milana.py
@@ -9,7 +9,7 @@ class Discord(Command):
 
     help_text = "переводчик с языка Миланы"
     help_texts = [
-        "- (пересланное сообщение) - переводит"
+        "- (пересланное сообщение) - переводит",
         "- (пересланное сообщение) + текст - парсит слова и добавляет новые в словарь"
     ]
 
@@ -21,22 +21,37 @@ class Discord(Command):
         if self.event.message.args:
             original_text = self.event.message.args
             new_words = self.add_new_words(milana_text, original_text)
+            if not new_words:
+                return "Не добавил новых слов"
             return "Добавил новые слова\n" + "\n".join(new_words)
         else:
             return self.translate(milana_text)
 
     @staticmethod
-    def add_new_words(milana_text, original_text):
+    def get_only_letters(text_list):
+        new_text_list = []
+        for word in text_list:
+            new_word = []
+            for letter in word:
+                if letter.isalpha():
+                    new_word.append(letter)
+            new_text_list.append("".join(new_word))
+        return new_text_list
+
+    def add_new_words(self, milana_text, original_text):
+        milana_text = self.get_only_letters(milana_text)
+        original_text = self.get_only_letters(original_text)
         new_words = {x[0]: x[1] for x in zip(milana_text, original_text) if x[0] != x[1]}
         created_words = []
         for new_word in new_words:
             translated_word = new_words[new_word]
-            asd, created = MilanaTranslate.objects.get_or_create(milana_text=new_word, translated_text=translated_word)
+            _, created = MilanaTranslate.objects.get_or_create(milana_text=new_word, translated_text=translated_word)
             if created:
                 created_words.append(f"{new_word} - {translated_word}")
         return created_words
 
     @staticmethod
+    # ToDo: обработка символов
     def translate(milana_text):
         translated = []
         for word in milana_text:
