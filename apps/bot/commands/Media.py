0c82dd82242bf7358657842859c488dbec932520
AndrewSha <Xoma163rus@gmail.com>
2022-05-12 11:09:55 +0400

    fixed thehole

--- apps/bot/commands/Media.py
+++ apps/bot/commands/Media.py
@@ -271,13 +271,22 @@ class Media(Command):
         bs4 = BeautifulSoup(content, 'html.parser')
         show_name = \
             [x.text for x in bs4.select('a[href*=shows]') if x.attrs['href'].startswith("/shows/") if x.text.strip()][0]
-        title = bs4.find('meta', attrs={'name': "og:title"}).attrs['content']
-        _id = url.split("/")[-1]
+
+        data_player = bs4.select_one('[data-player-source-value]').attrs
+        master_m3u8 = data_player['data-player-source-value']
+        title = data_player['data-player-title-value']
+        # title = bs4.find('meta', attrs={'name': "og:title"}).attrs['content']
+
+        _id = master_m3u8.split("/")[-2]
         prepend_text = f"https://video-cdn.the-hole.tv/episodes/{_id}"
 
         master_m3u8 = requests.get(f"{prepend_text}/master.m3u8").text
-        r = re.compile('1920x1080.*VIDEO-RANGE=SDR\n(.*)')
-        m3u8_1080p = r.findall(master_m3u8)[0]
+        master_m3u8_lines = master_m3u8.split('\n')
+        for i, line in enumerate(master_m3u8_lines):
+            if "1920x1080" in line:
+                m3u8_1080p = master_m3u8_lines[i + 1]
+                break
+        # m3u8_1080p = r.findall(master_m3u8)[0]
 
         original_video_m3u8 = f"{prepend_text}/{m3u8_1080p}"
         original_video_m3u8_list = requests.get(original_video_m3u8).text.split("\n")
