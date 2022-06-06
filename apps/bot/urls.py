b665812a76118e268bde40a8757341520f624072
AndrewSha <Xoma163rus@gmail.com>
2022-06-06 22:24:42 +0400

    typo

--- apps/bot/urls.py
+++ apps/bot/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path(f'tg/{env.str("TG_TOKEN")}', views.TelegramView.as_view()),
-    path(f'vk', views.VkView.as_view()),
+    path('vk', views.VkView.as_view()),
     path('yandex', views.yandex),
     path('api', views.APIView.as_view()),
 ]
