b3bbb4179e53e33080bd0def0823cca2c031b47f
AndrewSha <Xoma163rus@gmail.com>
2022-06-07 13:08:01 +0400

    send new message every 8 messages check if message is deleted

--- apps/bot/commands/Games/BullsAndCows.py
+++ apps/bot/commands/Games/BullsAndCows.py
@@ -98,8 +98,19 @@ class BullsAndCows(Command):
                 session.save()
 
                 if self.event.platform == Platform.TG:
-                    self.bot.parse_and_send_msgs({'text': session.message_body, 'message_id': session.message_id},
-                                                 self.event.peer_id)
+                    delta_messages = self.event.message.id - session.message_id
+                    print(delta_messages)
+                    if delta_messages > 8:
+                        old_msg_id = session.message_id
+                        r = self.bot.parse_and_send_msgs(session.message_body, self.event.peer_id)[0]
+                        message_id = r['response'].json()['result']['message_id']
+                        session.message_id = message_id
+                        session.save()
+                        self.bot.delete_message(self.event.peer_id, old_msg_id)
+                    r = self.bot.parse_and_send_msgs({'text': session.message_body, 'message_id': session.message_id},
+                                                     self.event.peer_id)
+                    if not r[0]['success']:
+                        self.bot.parse_and_send_msgs({'text': session.message_body}, self.event.peer_id)
                     self.bot.delete_message(self.event.peer_id, self.event.message.id)
                 else:
                     return new_msg
