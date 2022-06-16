c04f073395aa5b5ceb814428606618de555ed979
AndrewSha <Xoma163rus@gmail.com>
2022-06-16 14:37:17 +0400

    no edited_message

--- apps/bot/classes/events/TgEvent.py
+++ apps/bot/classes/events/TgEvent.py
@@ -33,15 +33,15 @@ class TgEvent(Event):
         if is_fwd:
             message = self.raw
         else:
-            edited_message = self.raw.get('edited_message')
+            # edited_message = self.raw.get('edited_message')
             callback_query = self.raw.get('callback_query')
             my_chat_member = self.raw.get('my_chat_member')
             if callback_query:
                 message = callback_query['message']
                 message['from'] = callback_query['from']
                 message['payload'] = callback_query['data']
-            elif edited_message:
-                message = edited_message
+            # elif edited_message:
+            #     message = edited_message
             elif my_chat_member:
                 message = my_chat_member
             else:
