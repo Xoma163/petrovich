5f97fef1d4080e532645178a6bc887d2689e0f90
AndrewSha <Xoma163rus@gmail.com>
2022-07-23 09:11:32 +0400

    move fleyva games

--- apps/service/management/commands/move_fleyva_games_to_2020.py
+++ apps/service/management/commands/move_fleyva_games_to_2020.py
@@ -1,13 +1,13 @@
-from datetime import timedelta
-
 from django.core.management.base import BaseCommand
 
+from apps.bot.models import Chat
 from apps.games.models import PetrovichGames
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        games = PetrovichGames.objects.filter(chat__pk=1, date__year=2021)
+        games = PetrovichGames.objects.filter(chat__pk=1)
+        new_chat = Chat.objects.get(pk=46)
         for game in games:
-            game.date = game.date - timedelta(days=365)
+            game.chat = new_chat
             game.save()
