3856622c8e3042c1e9c82c17e380795d910b1564
AndrewSha <Xoma163rus@gmail.com>
2022-09-05 23:30:54 +0400

    #399

--- apps/bot/commands/VoiceRecognition.py
+++ apps/bot/commands/VoiceRecognition.py
@@ -50,16 +50,71 @@ class VoiceRecognition(Command):
         AudioSegment.from_file(i, input_file_format).export(o, format='wav')
         o.seek(0)
 
-        r = sr.Recognizer()
+        r = PFilterGoogleRecognizer()
         with sr.AudioFile(o) as source:
             audio = r.record(source)
 
         reply_to = self.event.message.id
         try:
-            msg = r.recognize_google(audio, language='ru_RU')
+            msg = r.recognize_google(audio, language='ru_RU', pfilter=0)
             return {'text': msg, 'reply_to': reply_to}
         except sr.UnknownValueError:
             raise PWarning({'text': "Ничего не понял((", 'reply_to': reply_to})
         except sr.RequestError as e:
             print(str(e))
             raise PWarning("Проблема с форматом")
+
+
+# переопределение класса для отключения pFilter
+class PFilterGoogleRecognizer(sr.Recognizer):
+    def recognize_google(self, audio_data, key=None, language="en-US", show_all=False, pfilter=1):
+
+        assert isinstance(audio_data, sr.AudioData), "``audio_data`` must be audio data"
+        assert key is None or isinstance(key, str), "``key`` must be ``None`` or a string"
+        assert isinstance(language, str), "``language`` must be a string"
+
+        flac_data = audio_data.get_flac_data(
+            convert_rate=None if audio_data.sample_rate >= 8000 else 8000,  # audio samples must be at least 8 kHz
+            convert_width=2  # audio samples must be 16-bit
+        )
+        if key is None: key = "AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw"
+        url = "http://www.google.com/speech-api/v2/recognize?{}".format(sr.urlencode({
+            "client": "chromium",
+            "lang": language,
+            "key": key,
+            "pFilter": pfilter
+        }))
+        request = sr.Request(url, data=flac_data,
+                             headers={"Content-Type": "audio/x-flac; rate={}".format(audio_data.sample_rate)})
+
+        # obtain audio transcription results
+        try:
+            response = sr.urlopen(request, timeout=self.operation_timeout)
+        except sr.HTTPError as e:
+            raise sr.RequestError("recognition request failed: {}".format(e.reason))
+        except sr.URLError as e:
+            raise sr.RequestError("recognition connection failed: {}".format(e.reason))
+        response_text = response.read().decode("utf-8")
+
+        # ignore any blank blocks
+        actual_result = []
+        for line in response_text.split("\n"):
+            if not line: continue
+            result = sr.json.loads(line)["result"]
+            if len(result) != 0:
+                actual_result = result[0]
+                break
+
+        # return results
+        if show_all: return actual_result
+        if not isinstance(actual_result, dict) or len(
+                actual_result.get("alternative", [])) == 0: raise sr.UnknownValueError()
+
+        if "confidence" in actual_result["alternative"]:
+            # return alternative with highest confidence score
+            best_hypothesis = max(actual_result["alternative"], key=lambda alternative: alternative["confidence"])
+        else:
+            # when there is no confidence available, we arbitrarily choose the first hypothesis.
+            best_hypothesis = actual_result["alternative"][0]
+        if "transcript" not in best_hypothesis: raise sr.UnknownValueError()
+        return best_hypothesis["transcript"]
