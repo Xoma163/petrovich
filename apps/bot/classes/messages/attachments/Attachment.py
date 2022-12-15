19f0d325344b4356a2aa9c1850cfc7cc65a81fee
AndrewSha <Xoma163rus@gmail.com>
2022-12-15 18:08:34 +0400

    fixed bug

--- apps/bot/classes/messages/attachments/Attachment.py
+++ apps/bot/classes/messages/attachments/Attachment.py
@@ -29,7 +29,10 @@ class Attachment:
         self.name = None
 
     def set_private_download_url_tg(self, tg_bot, file_id):
-        file_path = tg_bot.requests.get('getFile', params={'file_id': file_id}).json()['result']['file_path']
+        r = tg_bot.requests.get('getFile', params={'file_id': file_id}).json()
+        if r.status_code != 200:
+            return
+        file_path = r['result']['file_path']
         self.private_download_url = f'https://{tg_bot.requests.API_TELEGRAM_URL}/file/bot{tg_bot.token}/{file_path}'
 
     def prepare_obj(self, file_like_object, allowed_exts_url=None, filename=None, guarantee_url=False):
