adc23f280e152e16b6beea2886c3dd795f241c02
AndrewSha <Xoma163rus@gmail.com>
2023-02-16 17:36:46 +0400

    #495

--- apps/bot/commands/FlaivaCommands/AdminTitle.py
+++ apps/bot/commands/FlaivaCommands/AdminTitle.py
@@ -9,6 +9,7 @@ class AdminTitle(Nostalgia):
     help_text = "меняет должность в чате флейвы"
     help_texts = [
         "- сбрасывает вашу должность",
+        "(должность) - меняет вашу должность",
         "(пользователь) (должность) - меняет должность участнику флейвы"
     ]
     access = Role.FLAIVA
@@ -17,16 +18,21 @@ class AdminTitle(Nostalgia):
 
     def start(self):
         if self.event.message.args:
-            self.check_args(2)
-            name = self.event.message.args[0]
-            title = " ".join(self.event.message.args_case[1:])
-            if len(title) > 16:
-                raise PWarning(f"Максимальная длина должности - 16 символов, у вас - {len(title)}")
+            if len(self.event.message.args) == 1:
+                title = self.event.message.args_str_case
+                self.bot.set_chat_admin_title(self.event.chat.chat_id, self.event.user.user_id, title)
+                return f"Поменял вашу должность на {title}"
+            else:
+                self.check_args(2)
+                name = self.event.message.args[0]
+                title = " ".join(self.event.message.args_case[1:])
+                if len(title) > 16:
+                    raise PWarning(f"Максимальная длина должности - 16 символов, у вас - {len(title)}")
 
-            profile = self.bot.get_profile_by_name(name, self.event.chat)
-            user_id = profile.get_user_by_platform(Platform.TG).user_id
-            self.bot.set_chat_admin_title(self.event.chat.chat_id, user_id, title)
-            return f"Поменял должность пользователю {profile} на {title}"
+                profile = self.bot.get_profile_by_name(name, self.event.chat)
+                user_id = profile.get_user_by_platform(Platform.TG).user_id
+                self.bot.set_chat_admin_title(self.event.chat.chat_id, user_id, title)
+                return f"Поменял должность пользователю {profile} на {title}"
         else:
-            self.bot.set_chat_admin_title(self.event.chat.chat_id, self.event.user.user_id, "")
+            self.bot.set_chat_admin_title(self.event.chat.chat_id, self.event.user.user_id, "ㅤ")
             return f"Сбросил вашу должность"
