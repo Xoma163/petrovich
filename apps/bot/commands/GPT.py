c3eac91ee86a82fa3795b49dd7b8cc45f53f727c
AndrewSha <Xoma163rus@gmail.com>
2023-08-17 18:37:59 +0400

    rollback streaming activity

--- apps/bot/commands/GPT.py
+++ apps/bot/commands/GPT.py
@@ -3,9 +3,9 @@ import re
 import openai
 
 from apps.bot.classes.Command import Command
+from apps.bot.classes.consts.ActivitiesEnum import ActivitiesEnum
 from apps.bot.classes.consts.Consts import Role
 from apps.bot.classes.consts.Exceptions import PWarning
-from apps.bot.classes.messages.ResponseMessage import ResponseMessageItem
 from petrovich.settings import env
 
 
@@ -32,6 +32,7 @@ class GPT(Command):
 
         request_text = " ".join(self.event.message.args_case[1:])
         try:
+            self.bot.set_activity_thread(self.event.peer_id, ActivitiesEnum.UPLOAD_PHOTO)
             response = openai.Image.create(
                 prompt=request_text,
                 n=5,  # images count
@@ -39,6 +40,8 @@ class GPT(Command):
             )
         except openai.error.APIError:
             raise PWarning("Какая-то непредвиденная ошибка. Попробуйте ещё раз")
+        finally:
+            self.bot.stop_activity_thread()
 
         attachments = [self.bot.get_photo_attachment(x['url']) for x in response["data"]]
         return {'text': f'Результат генерации по запросу "{request_text}"', 'attachments': attachments,
@@ -50,33 +53,21 @@ class GPT(Command):
         openai.api_key = env.str("CHIMERA_SECRET_KEY")
         openai.api_base = "https://chimeragpt.adventblocks.cc/api/v1"
         try:
+            self.bot.set_activity_thread(self.event.peer_id, ActivitiesEnum.TYPING)
             response = openai.ChatCompletion.create(
                 model='gpt-3.5-turbo-16k',
                 messages=[
                     {'role': 'user', 'content': request_text},
                 ],
-                stream=True,
                 allow_fallback=True
             )
         except openai.error.APIError:
             raise PWarning("Какая-то непредвиденная ошибка. Попробуйте ещё раз")
-
-        result = ""
-        update_per_symbols = 100
-        counter = update_per_symbols
-        message_id = None
-
-        for chunk in response:
-            result += chunk.choices[0].delta.get("content", "")
-            if len(result) > counter:
-                if message_id is None:
-                    message_id = self.send_message(result)
-                else:
-                    self.update_message(result, message_id)
-                counter += update_per_symbols
-
+        finally:
+            self.bot.stop_activity_thread()
+        result = response.choices[0].message.content
         result = self.replace_specsymbols_to_tg_format(result)
-        return {'text': result, 'message_id': message_id}
+        return {'text': result, 'reply_to': self.event.message.id}
 
     def replace_specsymbols_to_tg_format(self, text):
         p = re.compile(r'\*\*(.*)\*\*')  # markdown bold
@@ -87,15 +78,3 @@ class GPT(Command):
             tg_bold_text = self.bot.get_bold_text(bold_text).replace("**", '')
             text = text[:start_pos] + tg_bold_text + text[end_pos:]
         return text
-
-    def send_message(self, msg):
-        rm = ResponseMessageItem(msg, self.event.peer_id, self.event.message_thread_id)
-        rm.reply_to = self.event.message.id
-        r = self.bot.send_response_message_item(rm)
-        message_id = r.json()['result']['message_id']
-        return message_id
-
-    def update_message(self, msg, message_id):
-        rm = ResponseMessageItem(msg, self.event.peer_id, self.event.message_thread_id)
-        rm.message_id = message_id
-        self.bot.send_response_message_item(rm)
