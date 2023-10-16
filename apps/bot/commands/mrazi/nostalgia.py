4ce46d9544795a1f4af480bb73431ab0e9140955
AndrewSha <Xoma163rus@gmail.com>
2023-10-16 22:50:52 +0400

    fixed nostalgia attachments, removed indexes

--- apps/bot/commands/mrazi/nostalgia.py
+++ apps/bot/commands/mrazi/nostalgia.py
@@ -2,6 +2,7 @@ import json
 import random
 from datetime import datetime
 from io import BytesIO
+from urllib.parse import urlparse
 
 from apps.bot.classes.bots.tg_bot import TgBot
 from apps.bot.classes.command import Command
@@ -11,7 +12,6 @@ from apps.bot.classes.messages.response_message import ResponseMessage, Response
 from apps.bot.models import Profile, Bot
 from apps.bot.utils.quotes_generator import QuotesGenerator
 from apps.bot.utils.utils import get_urls_from_text
-from apps.service.models import Service
 
 
 class Nostalgia(Command):
@@ -20,8 +20,6 @@ class Nostalgia(Command):
     help_texts = [
         "- присылает 10 случайных сообщений",
         "(N,M=10) - присылает сообщения с позиции N до M. Максимальная разница между N и M - 200",
-        "(до) - присылает несколько сообщений до",
-        "(после) - присылает несколыько сообщений после",
         "(вложения) - присылает вложения со скриншота",
         "(фраза) - ищет фразу по переписке",
         "поиск (фраза) [N=1] - ищет фразу по переписке. N - номер страницы"
@@ -68,8 +66,6 @@ class Nostalgia(Command):
                     pass
 
         menu = [
-            [["до"], self.menu_before],
-            [["после"], self.menu_after],
             [['вложения'], self.menu_attachments],
             [['поиск'], self.menu_search],
             [['default'], self.menu_default]
@@ -77,28 +73,50 @@ class Nostalgia(Command):
         rmi = self.handle_menu(menu, arg0)()
         return ResponseMessage(rmi)
 
-    def menu_before(self) -> ResponseMessageItem:
-        index_from, index_to = self._get_indexes_from_db()
-        diff = index_to - index_from + 1
-        return self.menu_range(index_to - diff, index_from - diff)
-
-    def menu_after(self) -> ResponseMessageItem:
-        index_from, index_to = self._get_indexes_from_db()
-        diff = index_to - index_from + 1
-        return self.menu_range(index_to + diff, index_from + diff)
-
     def menu_attachments(self) -> ResponseMessageItem:
         data = self._load_file()
-        index_from, index_to = self._get_indexes_from_db()
+        self.check_args(3)
+        self.int_args = [1, 2]
+        self.parse_int()
+        index_from, index_to = self.event.message.args[1:3]
         msgs = data[index_from - 1: index_to]
-        all_atts = []
+        all_urls = []
         for msg in msgs:
             for att in msg['attachments']:
                 if 'link' in att:
-                    all_atts.append(self.bot.get_formatted_url("Ссылка", att['link']))
-            all_atts += get_urls_from_text(msg['text'])
-        answer = "\n".join(all_atts)
-        return ResponseMessageItem(text=answer)
+                    all_urls.append(att['link'])
+                else:
+                    print
+                all_urls += get_urls_from_text(msg['text'])
+        atts = []
+        texts = []
+        for url in all_urls:
+            att = None
+            ext = urlparse(url).path.rsplit('.')[-1]
+            if ext in ["jpg", "png", "jpeg"]:
+                link_name = "Фото"
+                att = self.bot.get_photo_attachment(url)
+            elif ext in ["gif", "gifv"]:
+                link_name = "Гиф"
+                att = self.bot.get_gif_attachment(url)
+            elif "youtu.be" in url or "youtube" in url:
+                link_name = "Ютуб"
+            elif "vk.com/video" in url:
+                link_name = "Вк видео"
+            elif "vk.com/doc" in url:
+                link_name = "Вк документ"
+            elif "vk.com/im" in url and "photo" in url:
+                link_name = "Вк фото"
+            else:
+                link_name = "Ссылка"
+            text = self.bot.get_formatted_url(link_name, url)
+            texts.append(text)
+            if att:
+                atts.append(att)
+        if not texts:
+            raise PWarning("Нет вложений")
+        answer = "\n".join(texts)
+        return ResponseMessageItem(text=answer, attachments=atts, disable_web_page_preview=True)
 
     def menu_default(self) -> ResponseMessageItem:
         if self.event.message.args:
@@ -166,8 +184,6 @@ class Nostalgia(Command):
             index_to = len(data) - 1
             index_from = index_to - diff
 
-        self._set_indexes_to_db(index_from, index_to)
-
         msgs = data[index_from - 1:index_to]
         msgs_parsed, has_att_link = self.prepare_msgs_for_quote_generator(msgs)
 
@@ -192,7 +208,7 @@ class Nostalgia(Command):
             button = self.bot.get_button("После", self.name, [index_from + diff, index_to + diff])
             buttons.append(button)
         if has_att_link:
-            button = self.bot.get_button("Вложения", self.name, ["вложения"])
+            button = self.bot.get_button("Вложения", self.name, ["вложения", index_from, index_to])
             buttons.append(button)
 
         keyboard = self.bot.get_inline_keyboard(buttons)
@@ -216,10 +232,7 @@ class Nostalgia(Command):
                 if counter >= lens[early_counter_index]:
                     counters[early_counter_index] -= 1
                 date = datetime.strptime(files[i][counters[i]]['datetime'], dt_format)
-                if not early_date:
-                    early_date = date
-                    early_counter_index = i
-                elif date < early_date:
+                if not early_date or date < early_date:
                     early_date = date
                     early_counter_index = i
             counter = counters[early_counter_index]
@@ -266,17 +279,3 @@ class Nostalgia(Command):
             new_msg = {'username': msg['author'], 'message': message, 'avatar': avatar}
             new_msgs.append(new_msg)
         return new_msgs, has_att_link
-
-    def _get_indexes_from_db(self):
-        index_from, _ = Service.objects.get_or_create(name=f'{self.KEY}_chats_index_from')
-        index_to, _ = Service.objects.get_or_create(name=f'{self.KEY}_chats_index_to')
-        return int(index_from.value), int(index_to.value)
-
-    def _set_indexes_to_db(self, index_from, index_to):
-        index_from_obj, _ = Service.objects.get_or_create(name=f'{self.KEY}_chats_index_from')
-        index_from_obj.value = index_from
-        index_from_obj.save()
-
-        index_to_obj, _ = Service.objects.get_or_create(name=f'{self.KEY}_chats_index_to')
-        index_to_obj.value = index_to
-        index_to_obj.save()
