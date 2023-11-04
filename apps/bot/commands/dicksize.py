a397dbb8f5706391096bc564dc6a7e3f929eaf06
AndrewSha <Xoma163rus@gmail.com>
2023-11-04 23:20:23 +0400

    dicksize update one message

--- apps/bot/commands/dicksize.py
+++ apps/bot/commands/dicksize.py
@@ -2,7 +2,9 @@ import datetime
 
 import numpy as np
 
+from apps.bot.classes.bots.tg_bot import TgBot
 from apps.bot.classes.command import Command
+from apps.bot.classes.const.consts import Platform
 from apps.bot.classes.const.exceptions import PSkip
 from apps.bot.classes.messages.response_message import ResponseMessage, ResponseMessageItem
 
@@ -12,22 +14,30 @@ class DickSize(Command):
 
     mentioned = True
 
+    platforms = [Platform.TG]
+
     def start(self) -> ResponseMessage:
+        self.bot: TgBot
+
         seed = int(datetime.datetime.now().strftime("%d%m%Y")) + self.event.sender.pk
         size = np.random.default_rng(seed=seed).normal(10, 2.2) + 3.21
         size = round(size, 1)
+
+        message_id = None
+        keyboard = None
 
         if self.event.is_from_chat:
             button = self.bot.get_button("Член", self.name)
             keyboard = self.bot.get_inline_keyboard([button])
             answer = f"{self.event.sender.name}, ваш член сегодня - {size}см"
 
-            if dicks := self.event.raw.get('callback_query', {}).get('message', {}).get('text'):
+            if message := self.event.raw.get('callback_query', {}).get('message', {}):
+                dicks = message['text']
                 if answer not in dicks:
                     answer = f"{dicks}\n{answer}"
+                    message_id = message['message_id']
                 else:
                     raise PSkip()
         else:
-            keyboard = None
             answer = f"Ваш член сегодня - {size}см"
-        return ResponseMessage(ResponseMessageItem(text=answer, keyboard=keyboard))
+        return ResponseMessage(ResponseMessageItem(text=answer, keyboard=keyboard, message_id=message_id))
