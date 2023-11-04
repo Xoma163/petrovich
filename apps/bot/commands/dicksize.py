5ac5e69273284ee8c9eb038820638fcf7db1f223
AndrewSha <Xoma163rus@gmail.com>
2023-11-04 23:36:23 +0400

    dicksize new message on new day

--- apps/bot/commands/dicksize.py
+++ apps/bot/commands/dicksize.py
@@ -1,8 +1,7 @@
-import datetime
+from datetime import datetime
 
 import numpy as np
 
-from apps.bot.classes.bots.tg_bot import TgBot
 from apps.bot.classes.command import Command
 from apps.bot.classes.const.consts import Platform
 from apps.bot.classes.const.exceptions import PSkip
@@ -11,33 +10,35 @@ from apps.bot.classes.messages.response_message import ResponseMessage, Response
 
 class DickSize(Command):
     name = "член"
-
     mentioned = True
-
     platforms = [Platform.TG]
 
     def start(self) -> ResponseMessage:
-        self.bot: TgBot
+        if self.event.is_from_pm:
+            answer = f"Ваш член сегодня - {self.get_dick_size()}см"
+            return ResponseMessage(ResponseMessageItem(text=answer))
 
-        seed = int(datetime.datetime.now().strftime("%d%m%Y")) + self.event.sender.pk
-        size = np.random.default_rng(seed=seed).normal(10, 2.2) + 3.21
-        size = round(size, 1)
+        button = self.bot.get_button("Член", self.name)
+        keyboard = self.bot.get_inline_keyboard([button])
+        answer = f"{self.event.sender.name}, ваш член сегодня - {self.get_dick_size()}см"
+
+        # Не была нажата кнопка
+        message = self.event.raw.get('callback_query', {}).get('message', {})
+        if not message:
+            return ResponseMessage(ResponseMessageItem(text=answer, keyboard=keyboard))
 
         message_id = None
-        keyboard = None
-
-        if self.event.is_from_chat:
-            button = self.bot.get_button("Член", self.name)
-            keyboard = self.bot.get_inline_keyboard([button])
-            answer = f"{self.event.sender.name}, ваш член сегодня - {size}см"
-
-            if message := self.event.raw.get('callback_query', {}).get('message', {}):
-                dicks = message['text']
-                if answer not in dicks:
-                    answer = f"{dicks}\n{answer}"
-                    message_id = message['message_id']
-                else:
-                    raise PSkip()
-        else:
-            answer = f"Ваш член сегодня - {size}см"
+        dicks = message['text']
+        today_message = datetime.fromtimestamp(message['date']).date() == datetime.now().date()
+        if today_message:
+            if answer not in dicks:
+                answer = f"{dicks}\n{answer}"
+                message_id = message['message_id']
+            else:
+                raise PSkip()
         return ResponseMessage(ResponseMessageItem(text=answer, keyboard=keyboard, message_id=message_id))
+
+    def get_dick_size(self) -> float:
+        seed = int(datetime.now().strftime("%d%m%Y")) + self.event.sender.pk
+        size = np.random.default_rng(seed=seed).normal(10, 2.2) + 3.21
+        return round(size, 1)
