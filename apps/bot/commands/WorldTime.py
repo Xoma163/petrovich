720e5aa35637299af8ab90faa33aff22345efab2
AndrewSha <Xoma163rus@gmail.com>
2023-11-12 23:48:00 +0400

    #598

--- apps/bot/commands/WorldTime.py
+++ apps/bot/commands/WorldTime.py
@@ -1,4 +1,5 @@
 import datetime
+from collections import Counter
 
 from apps.bot.classes.command import Command
 from apps.bot.classes.messages.response_message import ResponseMessage, ResponseMessageItem
@@ -12,14 +13,28 @@ class WorldTime(Command):
     help_texts = ["[город=из профиля] - текущее время в городе"]
 
     def start(self) -> ResponseMessage:
-        city = None
+        # args
         if self.event.message.args:
             city = City.objects.filter(synonyms__icontains=self.event.message.args[0]).first()
-        if not city:
-            city = self.event.sender.city
-        self.check_city(city)
+            answer = self._get_city_time_str(city)
+        # pm
+        elif self.event.is_from_pm:
+            self.check_city()
+            answer = self._get_city_time_str(self.event.sender.city)
+        # chat
+        else:
+            cities = [x.city for x in self.event.chat.users.all() if x.city]
+            x = Counter(cities)
+            answers = []
+            for city, _ in x.most_common():
+                answers.append(self._get_city_time_str(city))
+            answer = "\n".join(answers)
 
-        new_date = localize_datetime(datetime.datetime.utcnow(), city.timezone.name)
-        dt_str = new_date.strftime("%d.%m.%Y\n%H:%M:%S")
-        answer = f"Время в городе {city}\n\n{dt_str}"
         return ResponseMessage(ResponseMessageItem(text=answer))
+
+    @staticmethod
+    def _get_city_time_str(city: City) -> str:
+        new_date = localize_datetime(datetime.datetime.utcnow(), city.timezone.name)
+        dt_str = new_date.strftime("%H:%M:%S")
+        answer = f"Время в городе {city} — {dt_str}"
+        return answer
