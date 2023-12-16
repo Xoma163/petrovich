9b8432ba9d00ac87b84072a989d0a93e08226720
AndrewSha <Xoma163rus@gmail.com>
2023-12-16 22:52:00 +0400

    hide "Requirement already satisfied"

--- update_production.sh
+++ update_production.sh
@@ -5,8 +5,8 @@ git clean -fd
 git pull
 
 #npm run build
-venv/bin/python -m pip install --upgrade pip setuptools wheel
-venv/bin/pip install -r requirements.txt | grep -v 'already satisfied'
+venv/bin/python -m pip install --upgrade pip setuptools wheel  | grep -v 'Requirement already satisfied'
+venv/bin/pip install -r requirements.txt | grep -v 'Requirement already satisfied'
 venv/bin/python manage.py migrate
 venv/bin/python manage.py collectstatic --noinput
 
