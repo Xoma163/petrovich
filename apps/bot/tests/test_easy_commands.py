fbe28085cba3e80ed4f4bae4231b5d3a052f7797
AndrewSha <Xoma163rus@gmail.com>
2023-12-16 21:47:47 +0400

    remove legacy

--- apps/bot/tests/test_easy_commands.py
+++ apps/bot/tests/test_easy_commands.py
@@ -3,18 +3,15 @@ from apps.bot.commands.easy.bye import Bye
 from apps.bot.commands.easy.clear import Clear
 from apps.bot.commands.easy.documentation import Documentation
 from apps.bot.commands.easy.donate import Donate
-from apps.bot.commands.easy.gay_answer import GayAnswer
 from apps.bot.commands.easy.git import Git
 from apps.bot.commands.easy.google import Google
 from apps.bot.commands.easy.hi import Hi
 from apps.bot.commands.easy.issues import Issues
-from apps.bot.commands.easy.no import No
 from apps.bot.commands.easy.ping import Ping
 from apps.bot.commands.easy.sho import Sho
 from apps.bot.commands.easy.sorry_me import SorryMe
 from apps.bot.commands.easy.start_lada import StartLada
 from apps.bot.commands.easy.thanks import Thanks
-from apps.bot.commands.easy.yes import Yes
 from apps.bot.tests.bot_initializer import BotInitializer
 
 
@@ -49,13 +46,6 @@ class CommandDocumentationTestCase(BotInitializer):
 
 class CommandDonateTestCase(BotInitializer):
     Command = Donate
-
-    def test_no_args(self):
-        return self.check_correct_answer()
-
-
-class CommandGayAnswerTestCase(BotInitializer):
-    Command = GayAnswer
 
     def test_no_args(self):
         return self.check_correct_answer()
@@ -101,12 +91,6 @@ class CommandIssuesTestCase(BotInitializer):
         return self.check_correct_answer()
 
 
-class CommandNoTestCase(BotInitializer):
-    Command = No
-
-    def test_no_args(self):
-        return self.check_correct_answer()
-
 
 class CommandPingTestCase(BotInitializer):
     Command = Ping
@@ -138,13 +122,6 @@ class CommandStartLadaTestCase(BotInitializer):
 
 class CommandThanksTestCase(BotInitializer):
     Command = Thanks
-
-    def test_no_args(self):
-        return self.check_correct_answer()
-
-
-class CommandYesTestCase(BotInitializer):
-    Command = Yes
 
     def test_no_args(self):
         return self.check_correct_answer()
