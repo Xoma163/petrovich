4def4e062f0f015d0f9dd805af63b686572b3f71
AndrewSha <Xoma163rus@gmail.com>
2024-01-19 17:23:26 +0400

    switch

--- apps/service/management/commands/check_birthday.py
+++ apps/service/management/commands/check_birthday.py
@@ -1,16 +1,41 @@
+from datetime import datetime
+
 from django.core.management.base import BaseCommand
 
 from apps.bot.classes.bots.tg_bot import TgBot
-from apps.bot.models import Profile
-from apps.games.models import Gamer
+from apps.bot.classes.messages.response_message import ResponseMessageItem, ResponseMessage
+from apps.bot.models import Chat, Profile
 
 tg_bot = TgBot()
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for profile in Profile.objects.all():
-            if profile.gamer:
+        bday_chats = Chat.objects.filter(settings__celebrate_bday=True)
+
+        today = datetime.now()
+        profiles = Profile.objects.filter(
+            birthday__day=today.day,
+            birthday__month=today.month
+        )
+        for profile in profiles:
+            profile.gamer.roulette_points += 100000
+            profile.gamer.save()
+            if not profile.settings.celebrate_bday:
                 continue
-            gamer = Gamer(profile=profile)
-            gamer.save()
+
+            rmi = ResponseMessageItem(
+                f"С Днём рождения, {tg_bot.get_mention(profile)}!",
+                peer_id=profile.get_tg_user().user_id
+            )
+            rm = ResponseMessage(rmi)
+            tg_bot.send_response_message(rm)
+
+            bday_chats = profile.chats.filter(pk__in=[x.pk for x in bday_chats])
+            for chat in bday_chats:
+                rmi1 = ResponseMessageItem(
+                    f"С Днём рождения, {tg_bot.get_mention(profile)}!",
+                    peer_id=chat.chat_id
+                )
+                rm = ResponseMessage(rmi1)
+                tg_bot.send_response_message(rm)
