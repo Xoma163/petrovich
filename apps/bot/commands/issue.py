ef2829248bf81c83e044445e64749e2ad98c7010
AndrewSha <Xoma163rus@gmail.com>
2024-02-04 19:49:25 +0400

    #677

--- apps/bot/commands/issue.py
+++ apps/bot/commands/issue.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import Group
+
 from apps.bot.api.github.issue import GithubIssueAPI
 from apps.bot.api.imgur import Imgur
 from apps.bot.classes.command import Command
@@ -6,6 +8,7 @@ from apps.bot.classes.const.exceptions import PWarning
 from apps.bot.classes.help_text import HelpTextItem, HelpText, HelpTextItemCommand
 from apps.bot.classes.messages.attachments.photo import PhotoAttachment
 from apps.bot.classes.messages.response_message import ResponseMessage, ResponseMessageItem
+from apps.bot.models import Profile
 
 
 class Issue(Command):
@@ -76,11 +79,27 @@ class Issue(Command):
 
         issue = GithubIssueAPI()
         labels_in_github = [x for x in issue.get_all_labels() if x.lower() in tags] if tags else []
+        issue.author = self.event.sender
         issue.title = title
         issue.body = body
         issue.assignee = GithubIssueAPI.REPO_OWNER
         issue.labels = labels_in_github
         issue.create_in_github()
 
+        self.send_issue_info_to_admin(issue)
+
         answer = f"Отслеживать созданное ишю можно по {self.bot.get_formatted_url('ссылке', issue.remote_url)}"
         return ResponseMessage(ResponseMessageItem(text=answer))
+
+    def send_issue_info_to_admin(self, issue: GithubIssueAPI):
+
+        admin_group = Group.objects.get(name=Role.ADMIN.name)
+        profile = Profile.objects.filter(groups__in=[admin_group]).first()
+
+        if profile == issue.author:
+            return
+
+        answer = f"Новая иша {self.bot.get_formatted_url('тут', issue.remote_url)}"
+        rmi = ResponseMessageItem(answer)
+        rmi.peer_id = profile.get_tg_user().user_id
+        self.bot.send_response_message_item(rmi)
