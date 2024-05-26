c3ae4ea57d81a8aab770689550566fc0702a4f7d
AndrewSha <Xoma163rus@gmail.com>
2024-05-26 17:42:05 +0400

    dateutil

--- update_production.sh
+++ update_production.sh
@@ -6,7 +6,7 @@ git pull
 
 #npm run build
 venv/bin/python -m pip install --upgrade pip setuptools wheel  | grep -v 'Requirement already satisfied'
-venv/bin/pip install -r requirements.txt | grep -v 'Requirement already satisfied'
+poetry install # venv/bin/pip install -r requirements.txt | grep -v 'Requirement already satisfied'
 venv/bin/python manage.py migrate
 venv/bin/python manage.py collectstatic --noinput
 
