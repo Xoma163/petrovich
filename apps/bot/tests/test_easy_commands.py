47e7428f28bd17db75c7d862bdd50d54b036a175
AndrewSha <Xoma163rus@gmail.com>
2024-07-15 15:47:09 +0400

    tests

--- apps/bot/tests/test_easy_commands.py
+++ apps/bot/tests/test_easy_commands.py
@@ -1,14 +1,9 @@
 from apps.bot.classes.const.exceptions import PSkip
 from apps.bot.commands.easy.bye import Bye
-from apps.bot.commands.easy.clear import Clear
 from apps.bot.commands.easy.documentation import Documentation
 from apps.bot.commands.easy.donate import Donate
 from apps.bot.commands.easy.git import Git
 from apps.bot.commands.easy.hi import Hi
-from apps.bot.commands.easy.issues import Issues
-from apps.bot.commands.easy.ping import Ping
-from apps.bot.commands.easy.sho import Sho
-from apps.bot.commands.easy.start_lada import StartLada
 from apps.bot.commands.easy.thanks import Thanks
 from apps.bot.tests.bot_initializer import BotInitializer
 
@@ -26,13 +21,6 @@ class CommandByeTestCase(BotInitializer):
             return self.check_correct_answer()
         except PSkip:
             return True
-
-
-class CommandClearTestCase(BotInitializer):
-    Command = Clear
-
-    def test_no_args(self):
-        return self.check_correct_answer()
 
 
 class CommandDocumentationTestCase(BotInitializer):
@@ -69,34 +57,6 @@ class CommandHiTestCase(BotInitializer):
             return self.check_correct_answer()
         except PSkip:
             return True
-
-
-class CommandIssuesTestCase(BotInitializer):
-    Command = Issues
-
-    def test_no_args(self):
-        return self.check_correct_answer()
-
-
-class CommandPingTestCase(BotInitializer):
-    Command = Ping
-
-    def test_no_args(self):
-        return self.check_correct_answer()
-
-
-class CommandShoTestCase(BotInitializer):
-    Command = Sho
-
-    def test_no_args(self):
-        return self.check_correct_answer()
-
-
-class CommandStartLadaTestCase(BotInitializer):
-    Command = StartLada
-
-    def test_no_args(self):
-        return self.check_correct_answer()
 
 
 class CommandThanksTestCase(BotInitializer):
