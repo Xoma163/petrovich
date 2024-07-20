3e98c84a0c69e76d22613b7269652052fb3f9637
AndrewSha <Xoma163rus@gmail.com>
2024-07-20 18:12:30 +0400

    add model remove old models

--- apps/bot/api/gpt/models.py
+++ apps/bot/api/gpt/models.py
@@ -40,28 +40,27 @@ class GPTVoiceRecognitionModel(GPTModel):
 
 class GPTModels:
     # GPT-4
+    GPT_4_OMNI_MINI = GPTCompletionModel("gpt-4o-mini", "GPT-4 OMNI MINI", 0.15, 0.6)
     GPT_4_OMNI = GPTCompletionModel("gpt-4o", "GPT-4 OMNI", 5, 15)
-    GPT_4_TURBO = GPTCompletionModel("gpt-4-turbo-2024-04-09", "GPT-4 TURBO", 10, 30)
+    GPT_4_TURBO = GPTCompletionModel("gpt-4-turbo", "GPT-4 TURBO", 10, 30)
     GPT_4 = GPTCompletionModel("gpt-4", "GPT-4", 30, 60)
     GPT_4_32K = GPTCompletionModel("gpt-4-32k", "GPT-4 32K", 60, 120)
 
     # GPT-3.5
     GPT_3_5_TURBO_0125 = GPTCompletionModel("gpt-3.5-turbo-0125", "GPT-3.5 TURBO 0125", 0.5, 1.5)
+    GPT_3_5_TURBO_1106 = GPTCompletionModel("gpt-3.5-turbo-1106", "GPT-3.5 TURBO 1106", 1, 2)
+    GPT_3_5_TURBO_0613 = GPTCompletionModel("gpt-3.5-turbo-0613", "GPT-3.5 TURBO 0613", 1.5, 2)
+    GPT_3_5_TURBO_16K_0613 = GPTCompletionModel("gpt-3.5-turbo-16k-0613", "GPT-3.5 TURBO 16K 0613", 3, 4)
+    GPT_3_5_TURBO_0301 = GPTCompletionModel("gpt-3.5-turbo-0301", "GPT-3.5 TURBO 16K 0613", 1.5, 2)
 
     # image models
     DALLE_3 = GPTImageDrawModel("dall-e-3", "DALLE 3", image_cost=0.12)
-    DALLE_2 = GPTImageDrawModel("dall-e-2", "DALLE 2", image_cost=0.2)
+    DALLE_2 = GPTImageDrawModel("dall-e-2", "DALLE 2", image_cost=0.02)
 
     # Audio models
     WHISPER = GPTVoiceRecognitionModel("whisper-1", None, voice_recognition_1_min_cost=0.006)
 
     # older models
-    GPT_4_0125 = GPTCompletionModel("gpt-4-0125-preview", "GPT-4 0125", 10, 30)
-    GPT_4_1106 = GPTCompletionModel("gpt-4-1106-preview", "GPT-4 1106", 10, 30)
-    GPT_3_5_TURBO_1106 = GPTCompletionModel("gpt-3.5-turbo-1106", "GPT-3.5 TURBO 1106", 1, 2)
-    GPT_3_5_TURBO_0613 = GPTCompletionModel("gpt-3.5-turbo-0613", "GPT-3.5 TURBO 0613", 1.5, 2)
-    GPT_3_5_TURBO_16K_0613 = GPTCompletionModel("gpt-3.5-turbo-16k-0613", "GPT-3.5 TURBO 16K 0613", 3, 4)
-    GPT_3_5_TURBO_0301 = GPTCompletionModel("gpt-3.5-turbo-0301", "GPT-3.5 TURBO 16K 0613", 1.5, 2)
 
     @classmethod
     def get_all_models(cls, _class) -> list:
