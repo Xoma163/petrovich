9b5ff33546f9c461b3a11fa190ab97bdc341b1ab
AndrewSha <Xoma163rus@gmail.com>
2024-09-05 01:29:09 +0400

    u_tv

---
+++ apps/bot/api/media/u_tv.py
@@ -0,0 +1,37 @@
+import re
+from urllib.parse import urlparse
+
+import requests
+from bs4 import BeautifulSoup
+
+from apps.bot.api.media.data import VideoData
+from apps.bot.classes.messages.attachments.video import VideoAttachment
+from apps.bot.utils.video.video_handler import VideoHandler
+
+
+class UTV:
+    @staticmethod
+    def get_video_info(url: str) -> VideoData:
+        content = requests.get(url).content
+        bs4 = BeautifulSoup(content, features="lxml")
+        src = bs4.select_one(".project__frame").find("iframe").attrs['src']
+        parsed_url = urlparse(src)
+        new_url = f"{parsed_url[0]}://{parsed_url[1]}{parsed_url[2]}"
+
+        return VideoData(
+            title=bs4.select_one(".project__info-title").text,
+            channel_id=re.search(r'shows/([^/]+)/episodes', url).group(1),
+            channel_title=bs4.select_one(".project__single-name").text,
+            m3u8_master_url=f"{new_url}/master.m3u8",
+            thumbnail_url=bs4.select_one(f'a.swiper-slide[href="{url}"]').find('img').attrs['src'],
+            width=int(bs4.select_one('meta[property="og:image:width"]')['content']),
+            height=int(bs4.select_one('meta[property="og:image:height"]')['content'])
+        )
+
+    @staticmethod
+    def download(data: VideoData):
+        va = VideoAttachment()
+        va.m3u8_url = data.m3u8_master_url
+        vh = VideoHandler(video=va)
+        va.content = vh.download()
+        return va
---
+++ apps/bot/commands/media/services/u_tv.py
@@ -0,0 +1,37 @@
+from apps.bot.api.media.u_tv import UTV
+from apps.bot.classes.bots.chat_activity import ChatActivity
+from apps.bot.classes.const.activities import ActivitiesEnum
+from apps.bot.commands.media.service import MediaService, MediaServiceResponse
+from apps.bot.utils.utils import retry
+
+
+class UTVService(MediaService):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.use_proxy = True
+        self.service = UTV()
+
+    @retry(3, Exception, sleep_time=2)
+    def get_content_by_url(self, url: str) -> MediaServiceResponse:
+        with ChatActivity(self.bot, ActivitiesEnum.UPLOAD_VIDEO, self.event.peer_id):
+            return self._get_content_by_url(url)
+
+    def _get_content_by_url(self, url) -> MediaServiceResponse:
+        data = self.service.get_video_info(url)
+        text = f"{data.channel_title} - {data.title}"
+
+        if cached := self._get_cached(data.channel_id, data.video_id, text):
+            return cached
+
+        va = self.service.download(data)
+        va.width = data.width
+        va.height = data.height
+
+        if self.media_keys.cache or va.get_size_mb() > self.bot.MAX_VIDEO_SIZE_MB:
+            return self._cache_video(data.channel_id, data.video_id, text, va.content)
+        return MediaServiceResponse(text=text, attachments=[va], video_title=text)
+
+    @classmethod
+    def urls(cls) -> list[str]:
+        return ['u-tv.ru', 'www.u-tv.ru']
