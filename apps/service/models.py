facf3a41e8d06c4b37a662f7b2e9df991d7501b5
AndrewSha <Xoma163rus@gmail.com>
2024-10-26 17:23:50 +0400

    alter file field

--- apps/service/models.py
+++ apps/service/models.py
@@ -180,7 +180,7 @@ class VideoCache(TimeStampModelMixin):
     channel_id = models.CharField("ID канала", max_length=100)
     video_id = models.CharField("ID видео", max_length=100, null=True)
     filename = models.CharField('Название файла', max_length=1024)
-    video = models.FileField('Видео', blank=True, upload_to="service/video/")
+    video = models.FileField('Видео', blank=True, upload_to="service/video/", max_length=1024)
     original_url = models.URLField("Ссылка на оригинальное видео", blank=True, max_length=1024)
 
     class Meta:
