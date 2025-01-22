c7108ad134070f07befd2aff66ec34f178fdf14c
AndrewSha <Xoma163rus@gmail.com>
2025-01-22 22:59:31 +0400

    remove proxy

--- apps/bot/utils/proxy.py
+++ apps/bot/utils/proxy.py
@@ -8,10 +8,5 @@ def get_proxies():
 def get_http_proxies():
     return _get_proxies("PROXY_HTTP")
 
-
-def get_proxies_sweden():
-    return _get_proxies("PROXY_SOCKS5_SWEDEN")
-
-
 def _get_proxies(env_name: str):
     return {"https": env.str(env_name), "http": env.str(env_name)}
