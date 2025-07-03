63e9f979456834258207decc006f8ca69ed4b764
AndrewSha <Xoma163rus@gmail.com>
2025-07-03 12:26:32 +0400

    #980 #966

--- apps/gpt/commands/gpt/mixins/statistics.py
+++ apps/gpt/commands/gpt/mixins/statistics.py
@@ -1,5 +1,6 @@
 import datetime
 import io
+from zoneinfo import ZoneInfo
 
 import matplotlib.pyplot as plt
 from django.db.models import Q, Sum, QuerySet
@@ -14,6 +15,7 @@ from apps.bot.classes.help_text import HelpTextArgument, HelpTextKey
 from apps.bot.classes.messages.attachments.photo import PhotoAttachment
 from apps.bot.classes.messages.response_message import ResponseMessageItem
 from apps.bot.models import Profile
+from apps.bot.utils.utils import decl_of_num
 from apps.gpt.api.responses import GPTAPIResponse
 from apps.gpt.models import Usage, ProfileGPTSettings
 from apps.gpt.protocols import GPTCommandProtocol
@@ -21,7 +23,10 @@ from apps.gpt.protocols import GPTCommandProtocol
 
 class GPTStatisticsMixin(GPTCommandProtocol):
     STATISTICS_HELP_TEXT_ITEMS = [
-        HelpTextArgument("стата", "статистика по использованию"),
+        HelpTextArgument(
+            "стата [дата 1] [дата 2]",
+            "статистика по использованию. Если переданы аргументы, то пришлёт статистику за указаный период"
+        ),
     ]
     STATISTICS_KEY_ITEMS_KEY = [
         HelpTextKey(
@@ -38,26 +43,31 @@ class GPTStatisticsMixin(GPTCommandProtocol):
         Просмотр статистики по использованию
         """
         self.check_pm()
+        custom_range = self._get_custom_range_from_args()
 
         if self.event.message.is_key_provided({"ключ", "key"}):
             profiles = self._get_profiles_by_key()
-            answer = self._get_statistics_per_profile(profiles)
+            answer = self._get_statistics_per_profile(profiles, custom_range=custom_range)
         elif self.event.message.is_key_provided({"nokey"}):
             self.check_sender(Role.ADMIN)
             profiles = self._get_profiles_with_no_key()
-            answer = self._get_statistics_per_profile(profiles)
+            answer = self._get_statistics_per_profile(profiles, custom_range=custom_range)
         elif self.event.message.is_key_provided({"all"}):
             self.check_sender(Role.ADMIN)
             profiles = self._get_all_profiles()
-            answer = self._get_statistics_per_profile(profiles)
+            answer = self._get_statistics_per_profile(profiles, custom_range=custom_range)
         else:
             profiles = [self.event.sender]
-            answer = self._get_statistics_for_users(profiles)
+            answer, _ = self._get_statistics_for_users(
+                profiles,
+                return_custom_data=bool(custom_range),
+                custom_range=custom_range
+            )
 
         if not answer:
             raise PWarning("Ещё не было использований GPT")
 
-        plot_data = self._get_data_for_statistics_plot(profiles)
+        plot_data = self._get_data_for_statistics_plot(profiles, custom_range=custom_range)
         plot_bytes = self._get_statistics_plot(plot_data)
 
         statistics_plot_image = PhotoAttachment()
@@ -73,11 +83,13 @@ class GPTStatisticsMixin(GPTCommandProtocol):
             return_last_month_data: bool = True,
             return_current_month_data: bool = True,
             return_total_data: bool = True,
-    ) -> str | None:
+            return_custom_data: bool = False,
+            custom_range: tuple = None
+    ) -> tuple[str | None, float | None]:
         """Получение статистики по пользователям за разные периоды."""
         total_stats = self._get_stat_db_profile(Q(author__in=profiles))
         if not total_stats:
-            return None
+            return None, None
 
         now = timezone.now()
         current_month_start = now.replace(day=1)
@@ -102,10 +114,19 @@ class GPTStatisticsMixin(GPTCommandProtocol):
             )
             answer.append(f"{label} - $ {round(stats, 2)}")
 
-        if return_total_data:
-            answer.append(f"Всего - $ {round(total_stats, 2)}")
+        if return_custom_data:
+            start, end = custom_range
+            stats = self._get_stat_db_profile(
+                Q(author__in=profiles, created_at__gte=start, created_at__lt=end)
+            )
+            answer.append(
+                f"За период с {start.strftime('%d.%m.%Y')} по {end.strftime('%d.%m.%Y')} - $ {round(stats, 2)}")
 
-        return "\n".join(answer)
+        total_stats = round(total_stats, 2)
+        if return_total_data:
+            answer.append(f"Всего - $ {total_stats}")
+
+        return "\n".join(answer), total_stats
 
     # COMMON UTILS
 
@@ -153,39 +174,85 @@ class GPTStatisticsMixin(GPTCommandProtocol):
             raise PWarning("Не найдено пользователей")
         return profiles
 
-    def _get_statistics_per_profile(self, profiles: QuerySet[Profile]) -> str | None:
-        answers = []
+    def _get_statistics_per_profile(
+            self,
+            profiles: QuerySet[Profile],
+            custom_range: tuple[datetime, ...] | None = None
+    ) -> str | None:
+        stats_with_cost = []
         for profile in profiles:
-            profile_stats = self._get_statistics_for_users(
+            profile_stats, total_cost = self._get_statistics_for_users(
                 [profile],
                 return_today_data=False,
-                return_7_day_data=False
+                return_7_day_data=False,
+                return_custom_data=bool(custom_range),
+                custom_range=custom_range
             )
             if profile_stats:
-                answers.append(profile_stats)
-        if not answers:
+                stats_with_cost.append((profile_stats, total_cost))
+
+        if not stats_with_cost:
             return None
+        stats_with_cost.sort(key=lambda x: x[1], reverse=True)
+        answers = [stats[0] for stats in stats_with_cost]
 
         if profiles.count() > 1:
-            total_statistics = self._get_statistics_for_users(
+            total_statistics, _ = self._get_statistics_for_users(
                 list(profiles),
                 return_today_data=False,
-                return_7_day_data=False
+                return_7_day_data=False,
+                return_custom_data=bool(custom_range),
+                custom_range=custom_range
             )
             total_statistics = total_statistics.split('\n')
-            total_statistics[0] = "Всего:"
+            total_statistics[0] = "Сумма по всем:"
             total_statistics = "\n".join(total_statistics)
 
+            answers.append("-" * 10)
             answers.append(total_statistics)
         return "\n\n".join(answers)
 
+    def _get_custom_range_from_args(self) -> tuple[datetime.datetime, ...] | None:
+        if len(self.event.message.args) <= 1:
+            return None
+
+        start = self.event.message.args[1]
+        end = None
+        if len(self.event.message.args) > 2:
+            end = self.event.message.args[2]
+
+        tz = ZoneInfo('UTC')
+        try:
+            start_datetime = datetime.datetime.strptime(start, '%d.%m.%Y')
+            start_datetime = start_datetime.replace(tzinfo=tz)
+
+            if end:
+                end_datetime = datetime.datetime.strptime(end, '%d.%m.%Y')
+                end_datetime = end_datetime.replace(tzinfo=tz)
+
+            else:
+                end_datetime = timezone.now()
+        except ValueError:
+            raise PWarning("Ошибка в парсинге даты. Формат даты - ДД.ММ.ГГГГ")
+
+        if start_datetime > end_datetime:
+            start_datetime, end_datetime = end_datetime, start_datetime
+
+        return start_datetime, end_datetime
+
     @staticmethod
-    def _get_data_for_statistics_plot(profiles: list[Profile]):
-        one_month_ago = timezone.now() - datetime.timedelta(days=30)
+    def _get_data_for_statistics_plot(profiles: list[Profile], custom_range: tuple[datetime, ...] | None = None):
+        if not custom_range:
+            start = timezone.now() - datetime.timedelta(days=30)
+            end = timezone.now()
+        else:
+            start, end = custom_range
+        days_count = (end - start).days + 1
 
         usage_stats = Usage.objects.filter(
             author__in=profiles,
-            created_at__gte=one_month_ago
+            created_at__gte=start,
+            created_at__lt=end
         ).annotate(
             date=TruncDate('created_at')
         ).values('date').annotate(
@@ -193,13 +260,16 @@ class GPTStatisticsMixin(GPTCommandProtocol):
         ).order_by('date')
 
         stats_dict = {entry['date']: round(entry['total_cost'], 2) for entry in usage_stats}
-        all_dates = [one_month_ago.date() + datetime.timedelta(days=x) for x in range(31)]
+        all_dates = [start.date() + datetime.timedelta(days=x) for x in range(days_count)]
 
         return {date: stats_dict.get(date, float(0)) for date in all_dates}
 
     def _get_statistics_plot(self, stats_dict) -> bytes:
         dates = list(stats_dict.keys())
         costs = list(stats_dict.values())
+
+        days_count = len(dates)
+        days_decl = decl_of_num(days_count, ["день", "дня", "дней"])
 
         # Преобразование дат в формат datetime для matplotlib
         dates = [datetime.datetime.combine(date, datetime.time()) for date in dates]
@@ -218,7 +288,7 @@ class GPTStatisticsMixin(GPTCommandProtocol):
         plt.xticks(rotation=45)
 
         # Настройка заголовка и подписей осей
-        plt.title(f'Использование {self.provider.type_enum} за последние 30 дней', fontsize=16)
+        plt.title(f'Использование {self.provider.type_enum} за {days_count} {days_decl}', fontsize=16)
         plt.xlabel('Дата', fontsize=14)
         plt.ylabel('Стоимость, $', fontsize=14)
 
