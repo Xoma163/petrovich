6b301c39d90fc3441dc82b637d637f7f30da349e
AndrewSha <Xoma163rus@gmail.com>
2025-12-27 12:03:12 +0400

    deprecated vk

--- apps/bot/api/media/vk/video.py
+++ apps/bot/api/media/vk/video.py
@@ -27,7 +27,7 @@ class VKVideo(SubscribeService):
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'accept-language': 'ru-RU,ru;q=0.9',
         'cache-control': 'max-age=0',
-        'sec-ch-ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+        'sec-ch-ua': '"Google Chrome";v="143", "Chromium";v="143", "Not A(Brand";v="24""',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',
         'sec-fetch-dest': 'document',
@@ -35,7 +35,7 @@ class VKVideo(SubscribeService):
         'sec-fetch-site': 'none',
         'sec-fetch-user': '?1',
         'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36',
     }
 
     def __init__(self):
@@ -403,6 +403,7 @@ class VKVideo(SubscribeService):
         }
 
     def _get_playlist_data(self, url):
+        raise PError("Не работает")
         web_driver = get_web_driver(headers=self.HEADERS)
         try:
             web_driver.get(url)
