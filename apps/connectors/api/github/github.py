ffd05508a22a2b9b4389f1c40331e4fedfefaa13
AndrewSha <Xoma163rus@gmail.com>
2026-01-25 00:15:48 +0400

    refactor

---
+++ apps/connectors/api/github/github.py
@@ -0,0 +1,31 @@
+from apps.bot.core.messages.attachments.photo import PhotoAttachment
+from apps.connectors.api.handler import API
+from apps.connectors.api.imgdb import ImgdbAPI
+from petrovich.settings import env
+
+
+class GithubAPI(API):
+    TOKEN = env.str('GITHUB_TOKEN')
+    HEADERS = {
+        "Authorization": f"token {TOKEN}"
+    }
+
+    REPO_OWNER = 'Xoma163'
+    REPO_NAME = 'petrovich'
+
+    BASE_API_URL = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}"
+
+    ISSUES_URL = f'{BASE_API_URL}/issues'
+    LABELS_URL = f'{BASE_API_URL}/labels'
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    @staticmethod
+    def get_text_for_images_in_body(images: list[PhotoAttachment], log_filter=None) -> str:
+        result = []
+        i_api = ImgdbAPI(log_filter=log_filter)
+        for image in images:
+            image_url = i_api.upload_image(image)
+            result.append(f"![image]({image_url})")
+        return "\n" + "\n".join(result)
