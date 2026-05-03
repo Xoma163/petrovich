0a2c8b18f8561f89c27f8018e14b46dbe01bb969
AndrewSha <Xoma163rus@gmail.com>
2026-05-03 10:01:55 +0400

    update setup dependencies for uv and selenium

--- config/setup/setup.sh
+++ config/setup/setup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # install python and system depends
 sudo apt -y update
-sudo apt install -y python3.14 python3.14-venv python3-venv python3.14-dev python3-wheel postgresql libpq-dev ffmpeg nginx build-essential
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+sudo apt install -y libpq-dev gcc python3-dev redis ffmpeg chromium chromium-driver
 
 # venv and python depends
 # Создаём виртуальное окружение через uv.
 uv venv .venv
 set -e
 source ./.venv/bin/activate
-python -m pip install --upgrade pip uv
 uv sync
 
 # migrations and init db
